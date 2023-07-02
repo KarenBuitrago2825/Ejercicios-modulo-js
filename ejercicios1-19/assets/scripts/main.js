e7ea1e2aa92c73558609e8e6c58a097fdda10363
KarenBuitrago2825 <karenbuitrago2519@gmail.com>
2023-07-02 14:03:05 -0500

    prueba github

---
+++ ejercicios1-19/assets/scripts/main.js
@@ -0,0 +1,132 @@
+//Ejercicio Uno
+const miNombre = "Karen Ximena"
+console.log(miNombre)
+
+//Ejercicio Dos
+const miApellido = "Buitrago Figueredo"
+console.log(miApellido)
+
+//Ejercicio Tres
+let miEdad = 19
+console.log(miEdad)
+
+//Ejercicio Cuatro
+const miMascota = "Lulita"
+console.log(miMascota)
+
+// Ejercicio Cinco
+let edadMascota = 1
+console.log(edadMascota)
+
+//Ejercicio Siete
+const nombreCompleto = (miNombre + miApellido)
+console.log(nombreCompleto)
+
+//Ejercicio Ocho
+let textoPresentacion = (miNombre + " " + miApellido + " "+ miEdad +" "+ miMascota + " "+ edadMascota)
+console.log(textoPresentacion)
+
+//Ejercicio Nueve
+let sumaEdades = (miEdad + edadMascota)
+console.log(sumaEdades)
+
+let restaEdades = (miEdad - edadMascota)
+console.log(restaEdades)
+
+let productoEdades = (miEdad * edadMascota)
+console.log(productoEdades)
+
+let divisionEdades = (miEdad / edadMascota)
+console.log(divisionEdades)
+
+//Ejercicio Diez
+let alumno = {
+    nombre: "Karen",
+    apellido: "Buitrago",
+    edad: 19,
+    nacionalidad: "Colombiana",
+    peliculaFavorita: "Set it Up"
+}
+
+console.table(alumno)
+console.log(alumno.nombre)
+console.log(alumno.apellido)
+console.log(alumno.edad)
+console.log(alumno.nacionalidad)
+console.log(alumno.peliculaFavorita)
+
+//Ejercicio Once
+let mascota = {
+    nombre: "Lulita",
+    animal: "gatito",
+    color: "naranja",
+    personalidad: "perezoso",
+    edad:2
+}
+
+console.table(mascota)
+console.log(mascota.nombre)
+console.log(mascota.animal)
+console.log(mascota.color)
+console.log(mascota.personalidad)
+console.log(mascota.edad)
+
+//Ejercicio Doce
+const frutas =['Manzana' , 'Piña', 'Banana', 'Fresa', 'Pera']
+console.log(frutas)
+console.log(frutas[0])
+console.log(frutas[1])
+console.log(frutas[2])
+console.log(frutas[3])
+console.log(frutas[4])
+
+//Ejercicio Trece
+let textoAleatorio
+
+
+//Ejercicio Catorce
+const numeros = [1, 2, 3, 4, 5]
+console.log(numeros)
+console.log(numeros[0])
+console.log(numeros[1])
+console.log(numeros[2])
+console.log(numeros[3])
+console.log(numeros[4])
+
+//Ejercicio Quince
+const familia = ['Madre', 'Padre', 'Hijo', 'Hija', 'Perro']
+console.log(familia)
+console.log(familia[0])
+console.log(familia[1])
+console.log(familia[2])
+console.log(familia[3])
+console.log(familia[4])
+
+//Ejercicio Dieciséis
+// const soyMayorDeEdad = prompt('Ingrese su edad')
+// edadSuficiente = 18
+// let resultado = (soyMayorDeEdad == edadSuficiente)
+// const mensajeResultado = `Soy mayor de edad  pero no es igual a  ${edadSuficiente} porque mi edad es ${soyMayorDeEdad}`
+// console.log(mensajeResultado)
+
+//Ejercicio Diecisiete
+// const edad1 = prompt('Ingrese edad compañero uno')
+// const edad2 = prompt('Ingrese edad compañero dos')
+// const edadesIguales = (edad1 === edad2)
+// console.log(`La edad del compañero uno es igual a la del compañero dos ${edadesIguales}`) 
+// const soyMayor = (edad1 > edad2)
+// console.log(`La edad del compañero uno es mayor a la del compañero dos ${soyMayor}`)
+// const soyMenor = (edad1 < edad2)
+// console.log(`La edad del compañero uno es menor a la del compañero dos ${soyMenor}`)
+
+//Ejercicio Diecioscho
+// const edadUsuario = prompt("Ingrese su edad")
+// const altura = prompt("Ingrese su altura")
+// let puedeSubir = (edadUsuario == 6 && altura >= 120)
+// console.log(`Puede subir a la atraccion con su edad: ${edadUsuario}`)
+
+//Ejercicio Diecinueve
+
+
+
+
